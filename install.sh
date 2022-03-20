f8a699ababde11fa602561cbabc611c55539635b
Evripidis <s1754795@ed.ac.uk>
2022-03-20 23:46:15 +0000

    exp_run

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
+
+
 #!/bin/bash
 
 conda install numpy
 conda install pandas
 conda install seaborn
-conda install sklearn
+conda install scikit-learn
 conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
